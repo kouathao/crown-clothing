643c014b0f08e7ef8005a2ed4002188decead139
Koua Thao <kouathao09@gmail.com>
2020-02-10 00:12:48 -0600

    moved directory

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionOverview from "../../components/collections-overview/CollectionsOverview";
 
-const ShopPage = ({ collections }) => {
+import CollectionPage from "../collection/Collection";
+
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
