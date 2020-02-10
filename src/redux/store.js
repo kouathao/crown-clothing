6ac2f6384c619da03bfbf7bbe388cac8d084e53c
Koua Thao <kouathao09@gmail.com>
2020-02-09 23:20:55 -0600

    update shop and add collections overview component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
+
 // bring in root reducers
 import rootReducer from "./rootReducer";
 // bring in logger and put as an array
 const middlewares = [logger];
 
 // create store
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// create a new persist of the store for local storage
+export const persistor = persistStore(store);
+
+export default { store, persistor };
